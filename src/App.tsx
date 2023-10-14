49d1eb5732ce34df6ee204965965a943c8b2f546
benderang26 <benderang26@gmail.com>
2023-10-14 14:37:24 +0800

    created nav component with basic decoration

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,24 @@
+import React, { Fragment } from 'react'
 import { Container } from 'react-bootstrap';
 import { Route, Routes } from 'react-router-dom';
 import Home from './pages/Home';
 import Store from './pages/Store';
 import About from './pages/About';
+import Navbar from './components/Navbar';
+
 
 function App() {
   return (
-    <Container className='mb-4'>
-      <Routes>
-        <Route path="/" element={<Home />}/>
-        <Route path="/store" element={<Store />}/>
-        <Route path="/about" element={<About/>}/>
-      </Routes>
-    </Container>
+    <Fragment>
+      <Navbar />
+      <Container className='mb-4'>
+        <Routes>
+          <Route path="/" element={<Home />}/>
+          <Route path="/store" element={<Store />}/>
+          <Route path="/about" element={<About/>}/>
+        </Routes>
+      </Container>
+    </Fragment>
   );
 }
 
