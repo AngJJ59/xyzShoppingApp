8192d5dba25eabd348f0185148d16a26793e4c86
benderang26 <benderang26@gmail.com>
2023-10-14 13:47:50 +0800

    change index html element ui output

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+import { Navbar as NavBarBootstrap} from 'react-bootstrap'
+
+const Navbar = () => {
+  return (
+    <NavBarBootstrap className='bg-white shadow-sm mb-3'>
+        Navbar
+    </NavBarBootstrap>
+  )
+}
+
+export default Navbar
