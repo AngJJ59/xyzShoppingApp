49013ebc41c09230103246ed406c5ab0f843bfae
benderang26 <benderang26@gmail.com>
2023-10-19 23:08:20 +0800

    wip

--- src/pages/Store.tsx
+++ src/pages/Store.tsx
@@ -1,8 +1,27 @@
-import React from 'react'
+import React, { Fragment } from 'react'
 
 const Store = () => {
+  const temData = [
+    {
+      id: "1",
+      desc: "something1",
+      img: "/img1.jpg"
+    },
+    {
+      id: "2",
+      desc: "something2",
+      img: "/img2.jpg"
+    },
+    {
+      id: "3",
+      desc: "something3",
+      img: "/img3.jpg"
+    }
+  ]
   return (
-    <div>Store</div>
+    <Fragment>
+      
+    </Fragment>
   )
 }
 
